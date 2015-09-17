aae6d2053997e12518b3f1e3f0f2e639546e22d5
ccslakey <cromwellslakey@gmail.com>
2015-09-17 11:35:06 -0700

    first commit

---
+++ work_dat_interview.js
@@ -0,0 +1,33 @@
+Tasks = new Mongo.Collection("tasks");
+if (Meteor.isClient) {
+  
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
